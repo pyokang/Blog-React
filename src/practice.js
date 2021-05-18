d91a3a50ef122108b1572346e5d7d594fa267b39
Kang Pyo <PYO001@e.ntu.edu.sg>
2021-05-18 22:43:09 +0900

    First commit

---
+++ src/practice.js
@@ -0,0 +1,21 @@
+function sum (a, b) {
+    return a + b
+};
+
+let sum2 = (a, b) => {
+    return a + b
+}
+
+let sum3 = (a, b) => a + b
+
+function isPositive (number) {
+    return number >= 0
+}
+
+let isPositive2 = (number) => number >= 0
+
+let randomNumber = () => Math.random
+
+document.addEventListener('click', () => {
+    console.log('click')
+})
